96dfec723394a2b230ce83663744c83d911b4c65
Amrithaa M <emerald42rose@outlook.com>
2025-02-05 15:29:53 +0530

    Update program11.py

--- Lab-3/program11.py
+++ Lab-3/program11.py
@@ -1,1 +1,12 @@
+#tuples
+num = (10, 20, 30)
+student = ("John", 48, "Computer Science", 3.42)
+print(student)
 
+
+# Unpacking a Tuple
+a = ("RVU", 5000, "Computer Science and Engineering")
+(college, student, type_of_college) = a
+print(college)
+print(student)
+print(type_of_college)
