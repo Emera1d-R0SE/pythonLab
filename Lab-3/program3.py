2a613f225d422df4a84aca0e441b5a390c2c4ce9
Amrithaa M <emerald42rose@outlook.com>
2025-02-05 14:46:11 +0530

    Create program3.py

---
+++ Lab-3/program3.py
@@ -0,0 +1,8 @@
+#methods in strings
+name = " I am learning python " # it has whitespace on both sides
+print(name.upper())
+print(name.lower())
+print(name.strip())  # Removes whitespace from both ends
+print(name.lstrip())
+print(name.rstrip())
+print(name.replace("python","Data Science"))
