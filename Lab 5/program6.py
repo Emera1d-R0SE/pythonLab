442202d49a21df415b7581ef5f25be1d11a14a3d
Amrithaa M <emerald42rose@outlook.com>
2025-02-28 10:57:22 +0530

    Create program6.py

---
+++ Lab 5/program6.py
@@ -0,0 +1,8 @@
+#Example: Element-wise Multiplication
+import numpy as np
+arr = np.array([[1, 2], [3, 4]])
+result = arr * arr # Element-wise multiplication
+print(result)
+# Output:
+# [[1 4]
+# [9 16]]
